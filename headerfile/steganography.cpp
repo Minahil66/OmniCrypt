f9c453be32b8f0a3b73c925e47562e2a07815a3a
Minahil <minahil.066@outlook.com>
2026-05-06 21:15:04 +0500

    Create steganography.cpp

---
+++ headerfile/steganography.cpp
@@ -0,0 +1,56 @@
+#include "steganography.h"
+#include "COE.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+Steganography::Steganography(string n) : filename(n) {
+    ifstream infile(filename, ios::binary);
+    if(!infile.is_open()){
+        coolError("File failed to open");
+        return;
+    }
+    infile.seekg(0, ios::end);
+    size = infile.tellg();
+    imgdata.resize(size);
+    infile.seekg(0, ios::beg);
+    if(infile.fail()){
+        coolError("Failed to read data into memory");
+        return;
+    }
+    infile.read(imgdata.data(), size);
+    infile.close();
+    coolInfo("Successfully read file into memory");
+}
+
+void Steganography::HideMessage() {
+    string msg;
+    cout << CYAN << "Enter the message to hide: " << RESET;
+    getline(cin, msg);
+
+    vector<char> message(msg.begin(), msg.end());
+    message.push_back('\0');
+
+    if(message.size() > imgdata.size() - 0x36){
+        coolError("Message too long: Exceeds image size");
+        return;
+    }
+    for(int i = 0; i < message.size(); i++){
+        imgdata[0x36 + i] = message[i];
+    }
+    ofstream outfile("stega_" + filename, ios::binary);
+    outfile.write(imgdata.data(), imgdata.size());
+    outfile.close();
+    coolSuccess("Hidden in stega_" + filename);
+}
+
+void Steganography::ExtractMessage() {
+    vector<char> msg;
+    for(int i = 0x36; i <= imgdata.size(); i++){
+        if(imgdata[i] == '\0') break;
+        msg.push_back(imgdata[i]);
+    }
+    string message(msg.begin(), msg.end());
+    cout << GREEN << "Hidden message: " << BOLD << message << RESET << endl;
+}
