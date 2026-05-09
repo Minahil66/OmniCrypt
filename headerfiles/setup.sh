8db3c7ede7a4c818d17bcfd9bfb7e00edbdcdd99
Minahil <minahil.066@outlook.com>
2026-05-09 11:52:21 +0500

    Update setup.sh

--- headerfiles/setup.sh
+++ headerfiles/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "🔧 Compiling OmniCrypt..."
-g++ -std=c++11 main.cpp ciphers.cpp steganography.cpp encryption_tracker.cpp logger.h cascade_engine.h -o omnicrypt
+g++ -std=c++11 main.cpp ciphers.cpp steganography.cpp encryption_tracker.cpp logger.cpp cascade_engine.cpp -o omnicrypt
 echo " Done! Run with: ./omnicrypt"
